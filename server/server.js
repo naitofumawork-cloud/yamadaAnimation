10d080953b4bdba11a4d1886e0bf28d6cc05342c
naitofumawork-cloud <example@gmail.com>
2025-10-27 05:11:37 +0900

    初回コミット：ポートフォリオ用サイト

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// publicフォルダを静的ファイルとして公開
+app.use(express.static(path.join(__dirname, '../public')));
+
+// ルートアクセスでindex.htmlを返す
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/html/entrance.html'));//パス指定
+});
+
+// サーバー起動
+app.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
